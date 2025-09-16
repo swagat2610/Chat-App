ecfccf6429a7e4d493533c56eafe0fa3ad5c0185
swagat2610 <146967564+swagat2610@users.noreply.github.com>
2025-09-16 10:43:50 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -31,8 +31,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
 
     return res.status(201).json(user);
@@ -58,8 +58,8 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
 
     return res.status(200).json(user);
